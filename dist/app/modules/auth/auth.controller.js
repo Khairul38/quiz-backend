8feeb2945272cf3a29da2f232fc856dc07bda1d6
Khairul Alam <khairulislam38@gmail.com>
2023-12-23 12:33:05 +0600

    update auth, user and profile route

--- dist/app/modules/auth/auth.controller.js
+++ dist/app/modules/auth/auth.controller.js
@@ -1,27 +1,59 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __rest = (this && this.__rest) || function (s, e) {
+  };
+var __rest =
+  (this && this.__rest) ||
+  function (s, e) {
     var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+    for (var p in s)
+      if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
         t[p] = s[p];
     if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
-        }
+      for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+        if (
+          e.indexOf(p[i]) < 0 &&
+          Object.prototype.propertyIsEnumerable.call(s, p[i])
+        )
+          t[p[i]] = s[p[i]];
+      }
     return t;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.refreshTokenUser = exports.loginUser = exports.createUser = void 0;
 const http_status_1 = __importDefault(require("http-status"));
@@ -29,46 +61,54 @@ const config_1 = __importDefault(require("../../../config"));
 const catchAsync_1 = __importDefault(require("../../../shared/catchAsync"));
 const sendResponse_1 = __importDefault(require("../../../shared/sendResponse"));
 const auth_service_1 = require("./auth.service");
-exports.createUser = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+exports.createUser = (0, catchAsync_1.default)((req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     const user = __rest(req.body, []);
     const result = yield (0, auth_service_1.createUserToDB)(user);
     (0, sendResponse_1.default)(res, {
-        success: true,
-        statusCode: http_status_1.default.OK,
-        message: "User signup successfully",
-        data: result,
+      success: true,
+      statusCode: http_status_1.default.OK,
+      message: "User signup successfully",
+      data: result,
     });
-}));
-exports.loginUser = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+  })
+);
+exports.loginUser = (0, catchAsync_1.default)((req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     const loginData = __rest(req.body, []);
     const result = yield (0, auth_service_1.loginUserFromDB)(loginData);
     const { refreshToken, accessToken } = result;
     // set refresh token into cookie
     const cookieOptions = {
-        secure: config_1.default.env === "production",
-        httpOnly: true,
+      secure: config_1.default.env === "production",
+      httpOnly: true,
     };
     res.cookie("refreshToken", refreshToken, cookieOptions);
     (0, sendResponse_1.default)(res, {
-        statusCode: http_status_1.default.OK,
-        success: true,
-        message: "User signin successfully !",
-        token: accessToken,
+      statusCode: http_status_1.default.OK,
+      success: true,
+      message: "User signin successfully !",
+      token: accessToken,
     });
-}));
-exports.refreshTokenUser = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+  })
+);
+exports.refreshTokenUser = (0, catchAsync_1.default)((req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     const { refreshToken } = req.cookies;
-    const result = yield (0, auth_service_1.refreshTokenUserFromDB)(refreshToken);
+    const result = yield (0, auth_service_1.refreshTokenUserFromDB)(
+      refreshToken
+    );
     // set refresh token into cookie
     const cookieOptions = {
-        secure: config_1.default.env === "production",
-        httpOnly: true,
+      secure: config_1.default.env === "production",
+      httpOnly: true,
     };
     res.cookie("refreshToken", refreshToken, cookieOptions);
     (0, sendResponse_1.default)(res, {
-        statusCode: http_status_1.default.OK,
-        success: true,
-        message: "New user access token generated successfully !",
-        data: result,
+      statusCode: http_status_1.default.OK,
+      success: true,
+      message: "New user access token generated successfully !",
+      data: result,
     });
-}));
+  })
+);
