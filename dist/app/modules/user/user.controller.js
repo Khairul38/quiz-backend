be40fc55f70e3100ad90a032d3148d0aec9a20e2
Khairul Alam <khairulislam38@gmail.com>
2023-12-23 12:42:31 +0600

    update readme

--- dist/app/modules/user/user.controller.js
+++ dist/app/modules/user/user.controller.js
@@ -1,48 +1,18 @@
 "use strict";
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator["throw"](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done
-          ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.deleteSingleUser =
-  exports.updateSingleUser =
-  exports.getSingleUser =
-  exports.getAllUser =
-    void 0;
+exports.deleteSingleUser = exports.updateSingleUser = exports.getSingleUser = exports.getAllUser = void 0;
 const http_status_1 = __importDefault(require("http-status"));
 const pagination_1 = require("../../../constants/pagination");
 const catchAsync_1 = __importDefault(require("../../../shared/catchAsync"));
@@ -50,62 +20,45 @@ const pick_1 = __importDefault(require("../../../shared/pick"));
 const sendResponse_1 = __importDefault(require("../../../shared/sendResponse"));
 const user_constant_1 = require("./user.constant");
 const user_service_1 = require("./user.service");
-exports.getAllUser = (0, catchAsync_1.default)((req, res) =>
-  __awaiter(void 0, void 0, void 0, function* () {
-    const filters = (0, pick_1.default)(
-      req.query,
-      user_constant_1.userFilterableFields
-    );
-    const pagination = (0, pick_1.default)(
-      req.query,
-      pagination_1.paginationFields
-    );
-    const result = yield (0, user_service_1.getAllUserFromDB)(
-      filters,
-      pagination
-    );
+exports.getAllUser = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const filters = (0, pick_1.default)(req.query, user_constant_1.userFilterableFields);
+    const pagination = (0, pick_1.default)(req.query, pagination_1.paginationFields);
+    const result = yield (0, user_service_1.getAllUserFromDB)(filters, pagination);
     (0, sendResponse_1.default)(res, {
-      statusCode: http_status_1.default.OK,
-      success: true,
-      message: "All user fetched successfully",
-      meta: result.meta,
-      data: result.data,
+        statusCode: http_status_1.default.OK,
+        success: true,
+        message: "All user fetched successfully",
+        meta: result.meta,
+        data: result.data,
     });
-  })
-);
-exports.getSingleUser = (0, catchAsync_1.default)((req, res) =>
-  __awaiter(void 0, void 0, void 0, function* () {
+}));
+exports.getSingleUser = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const result = yield (0, user_service_1.getSingleUserFromDB)(req.params.id);
     (0, sendResponse_1.default)(res, {
-      statusCode: http_status_1.default.OK,
-      success: true,
-      message: "Single user fetched successfully",
-      data: result,
+        statusCode: http_status_1.default.OK,
+        success: true,
+        message: "Single user fetched successfully",
+        data: result,
     });
-  })
-);
-exports.updateSingleUser = (0, catchAsync_1.default)((req, res) =>
-  __awaiter(void 0, void 0, void 0, function* () {
+}));
+exports.updateSingleUser = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
     const payload = req.body;
     const result = yield (0, user_service_1.updateSingleUserToDB)(id, payload);
     (0, sendResponse_1.default)(res, {
-      statusCode: http_status_1.default.OK,
-      success: true,
-      message: "User updated successfully",
-      data: result,
+        statusCode: http_status_1.default.OK,
+        success: true,
+        message: "User updated successfully",
+        data: result,
     });
-  })
-);
-exports.deleteSingleUser = (0, catchAsync_1.default)((req, res) =>
-  __awaiter(void 0, void 0, void 0, function* () {
+}));
+exports.deleteSingleUser = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
     const result = yield (0, user_service_1.deleteSingleUserFromDB)(id);
     (0, sendResponse_1.default)(res, {
-      statusCode: http_status_1.default.OK,
-      success: true,
-      message: "User deleted successfully",
-      data: result,
+        statusCode: http_status_1.default.OK,
+        success: true,
+        message: "User deleted successfully",
+        data: result,
     });
-  })
-);
+}));
