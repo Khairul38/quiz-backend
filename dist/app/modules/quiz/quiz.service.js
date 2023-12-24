5db2c08f9504fe069f26e2e0b6ffbf799348d62a
Khairul Alam <khairulislam38@gmail.com>
2023-12-25 02:54:37 +0600

    update

--- dist/app/modules/quiz/quiz.service.js
+++ dist/app/modules/quiz/quiz.service.js
@@ -30,7 +30,7 @@ const paginationHelper_1 = require("../../../helpers/paginationHelper");
 const prisma_1 = require("../../../shared/prisma");
 const quiz_constant_1 = require("./quiz.constant");
 const createQuizToDB = (user, quizData) => __awaiter(void 0, void 0, void 0, function* () {
-    const { categoryId, mark, multiChoice, question, timeTaken, quizAnswers, } = quizData;
+    const { categoryId, mark, multiChoice, question, timeTaken, quizAnswers } = quizData;
     const newQuiz = yield prisma_1.prisma.$transaction((transactionClient) => __awaiter(void 0, void 0, void 0, function* () {
         const result = yield transactionClient.quiz.create({
             data: {
