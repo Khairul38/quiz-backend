6fea8b20293c31c65a797a60938f7c0335c54aa0
Khairul Alam <khairulislam38@gmail.com>
2023-12-25 03:03:18 +0600

    fix build

--- dist/app/modules/user/user.constant.js
+++ dist/app/modules/user/user.constant.js
@@ -2,4 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.userFilterableFields = exports.userSearchableFields = void 0;
 exports.userSearchableFields = ["name", "address"];
-exports.userFilterableFields = ["searchTerm", "email", "role", "contactNo"];
+exports.userFilterableFields = [
+    "searchTerm",
+    "email",
+    "role",
+    "contactNo",
+];
