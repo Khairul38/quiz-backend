82b35dea66189a817daf2c448fbc71be6bdc042e
Khairul Alam <khairulislam38@gmail.com>
2023-12-25 03:02:33 +0600

    fix some issue

--- dist/app/modules/leaderBoard/leaderBoard.service.js
+++ dist/app/modules/leaderBoard/leaderBoard.service.js
@@ -1,141 +1,195 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __rest = (this && this.__rest) || function (s, e) {
+  };
+var __rest =
+  (this && this.__rest) ||
+  function (s, e) {
     var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+    for (var p in s)
+      if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
         t[p] = s[p];
     if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
-        }
+      for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+        if (
+          e.indexOf(p[i]) < 0 &&
+          Object.prototype.propertyIsEnumerable.call(s, p[i])
+        )
+          t[p[i]] = s[p[i]];
+      }
     return t;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.deleteSingleLeaderBoardFromDB = exports.updateSingleLeaderBoardToDB = exports.getSingleLeaderBoardFromDB = exports.getAllLeaderBoardFromDB = exports.createLeaderBoardToDB = void 0;
+exports.deleteSingleLeaderBoardFromDB =
+  exports.updateSingleLeaderBoardToDB =
+  exports.getSingleLeaderBoardFromDB =
+  exports.getAllLeaderBoardFromDB =
+  exports.createLeaderBoardToDB =
+    void 0;
 const http_status_1 = __importDefault(require("http-status"));
 const ApiError_1 = __importDefault(require("../../../errors/ApiError"));
 const paginationHelper_1 = require("../../../helpers/paginationHelper");
 const prisma_1 = require("../../../shared/prisma");
 const leaderBoard_constant_1 = require("./leaderBoard.constant");
-const createLeaderBoardToDB = (leaderBoardData) => __awaiter(void 0, void 0, void 0, function* () {
+const createLeaderBoardToDB = leaderBoardData =>
+  __awaiter(void 0, void 0, void 0, function* () {
     const result = yield prisma_1.prisma.leaderBoard.create({
-        data: leaderBoardData,
-        include: {
-            user: true,
-        },
+      data: leaderBoardData,
+      include: {
+        user: true,
+      },
     });
     return result;
-});
+  });
 exports.createLeaderBoardToDB = createLeaderBoardToDB;
-const getAllLeaderBoardFromDB = (filters, paginationOptions) => __awaiter(void 0, void 0, void 0, function* () {
-    const { page, size, skip, sortBy, sortOrder } = (0, paginationHelper_1.calculatePagination)(paginationOptions);
-    const { searchTerm } = filters, filtersData = __rest(filters, ["searchTerm"]);
+const getAllLeaderBoardFromDB = (filters, paginationOptions) =>
+  __awaiter(void 0, void 0, void 0, function* () {
+    const { page, size, skip, sortBy, sortOrder } = (0,
+    paginationHelper_1.calculatePagination)(paginationOptions);
+    const { searchTerm } = filters,
+      filtersData = __rest(filters, ["searchTerm"]);
     const andCondition = [];
     if (searchTerm) {
-        andCondition.push({
-            OR: leaderBoard_constant_1.leaderBoardSearchableFields.map(field => ({
-                [field]: {
-                    contains: searchTerm,
-                    mode: "insensitive",
-                },
-            })),
-        });
+      andCondition.push({
+        OR: leaderBoard_constant_1.leaderBoardSearchableFields.map(field => ({
+          [field]: {
+            contains: searchTerm,
+            mode: "insensitive",
+          },
+        })),
+      });
     }
     if (Object.keys(filtersData).length > 0) {
-        andCondition.push({
-            AND: Object.keys(filtersData).map(key => ({
-                [key]: {
-                    equals: filtersData[key],
-                },
-            })),
-        });
+      andCondition.push({
+        AND: Object.keys(filtersData).map(key => ({
+          [key]: {
+            equals: filtersData[key],
+          },
+        })),
+      });
     }
     const whereCondition = andCondition.length > 0 ? { AND: andCondition } : {};
     const result = yield prisma_1.prisma.leaderBoard.findMany({
-        where: whereCondition,
-        skip,
-        take: size,
-        orderBy: sortBy && sortOrder
-            ? {
-                [sortBy]: sortOrder,
+      where: whereCondition,
+      skip,
+      take: size,
+      orderBy:
+        sortBy && sortOrder
+          ? {
+              [sortBy]: sortOrder,
             }
-            : { createdAt: "desc" },
+          : { createdAt: "desc" },
     });
     const total = yield prisma_1.prisma.leaderBoard.count();
     const totalPage = Number(total) / Number(size);
     return {
-        meta: {
-            total,
-            page,
-            size,
-            totalPage: Math.ceil(totalPage),
-        },
-        data: result,
+      meta: {
+        total,
+        page,
+        size,
+        totalPage: Math.ceil(totalPage),
+      },
+      data: result,
     };
-});
+  });
 exports.getAllLeaderBoardFromDB = getAllLeaderBoardFromDB;
-const getSingleLeaderBoardFromDB = (id) => __awaiter(void 0, void 0, void 0, function* () {
+const getSingleLeaderBoardFromDB = id =>
+  __awaiter(void 0, void 0, void 0, function* () {
     const result = yield prisma_1.prisma.leaderBoard.findUnique({
-        where: {
-            id,
-        },
+      where: {
+        id,
+      },
     });
     if (result) {
-        return result;
+      return result;
+    } else {
+      throw new ApiError_1.default(
+        http_status_1.default.BAD_REQUEST,
+        "There is no leaderBoard with the id/Failed to fetched leaderBoard"
+      );
     }
-    else {
-        throw new ApiError_1.default(http_status_1.default.BAD_REQUEST, "There is no leaderBoard with the id/Failed to fetched leaderBoard");
-    }
-});
+  });
 exports.getSingleLeaderBoardFromDB = getSingleLeaderBoardFromDB;
-const updateSingleLeaderBoardToDB = (id, payload) => __awaiter(void 0, void 0, void 0, function* () {
+const updateSingleLeaderBoardToDB = (id, payload) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     if (payload.categoryId) {
-        const isExist = yield prisma_1.prisma.category.findUnique({
-            where: {
-                id: payload.categoryId,
-            },
-        });
-        if (!isExist) {
-            throw new ApiError_1.default(http_status_1.default.CONFLICT, "There is no category with this category Id. Please provide a valid category Id.");
-        }
+      const isExist = yield prisma_1.prisma.category.findUnique({
+        where: {
+          id: payload.categoryId,
+        },
+      });
+      if (!isExist) {
+        throw new ApiError_1.default(
+          http_status_1.default.CONFLICT,
+          "There is no category with this category Id. Please provide a valid category Id."
+        );
+      }
     }
     const result = yield prisma_1.prisma.leaderBoard.update({
-        where: {
-            id,
-        },
-        data: payload,
+      where: {
+        id,
+      },
+      data: payload,
     });
     if (result) {
-        return result;
+      return result;
+    } else {
+      throw new ApiError_1.default(
+        http_status_1.default.BAD_REQUEST,
+        "There is no leaderBoard with the id/Failed to update leaderBoard"
+      );
     }
-    else {
-        throw new ApiError_1.default(http_status_1.default.BAD_REQUEST, "There is no leaderBoard with the id/Failed to update leaderBoard");
-    }
-});
+  });
 exports.updateSingleLeaderBoardToDB = updateSingleLeaderBoardToDB;
-const deleteSingleLeaderBoardFromDB = (id) => __awaiter(void 0, void 0, void 0, function* () {
+const deleteSingleLeaderBoardFromDB = id =>
+  __awaiter(void 0, void 0, void 0, function* () {
     const result = yield prisma_1.prisma.leaderBoard.delete({
-        where: {
-            id,
-        },
+      where: {
+        id,
+      },
     });
     if (result) {
-        return result;
+      return result;
+    } else {
+      throw new ApiError_1.default(
+        http_status_1.default.BAD_REQUEST,
+        "There is no leaderBoard with the id/Failed to delete leaderBoard"
+      );
     }
-    else {
-        throw new ApiError_1.default(http_status_1.default.BAD_REQUEST, "There is no leaderBoard with the id/Failed to delete leaderBoard");
-    }
-});
+  });
 exports.deleteSingleLeaderBoardFromDB = deleteSingleLeaderBoardFromDB;
