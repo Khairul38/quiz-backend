32a947b0133a163ab87e99742c057a5e1de6567a
Khairul Alam <khairulislam38@gmail.com>
2023-12-25 01:51:57 +0600

    update

--- dist/app/modules/quiz/quiz.constant.js
+++ dist/app/modules/quiz/quiz.constant.js
@@ -1,7 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.quizFilterableFields = exports.quizSearchableFields = void 0;
-exports.quizSearchableFields = ["question", "creatorId", "categoryId"];
+exports.quizSearchableFields = [
+    "question",
+    "creatorId",
+    "categoryId",
+];
 exports.quizFilterableFields = [
     "searchTerm",
     "question",
