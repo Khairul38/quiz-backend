5cc92a017b3e99436c8ccc835ec1a35230dcb934
Khairul Alam <khairulislam38@gmail.com>
2023-12-25 20:46:57 +0600

    update

--- dist/app/modules/auth/auth.validation.js
+++ dist/app/modules/auth/auth.validation.js
@@ -3,19 +3,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.refreshTokenZodSchema = exports.loginZodSchema = void 0;
 const zod_1 = require("zod");
 exports.loginZodSchema = zod_1.z.object({
-    body: zod_1.z.object({
-        email: zod_1.z.string({
-            required_error: "Email is required",
-        }),
-        password: zod_1.z.string({
-            required_error: "Password is required",
-        }),
+  body: zod_1.z.object({
+    email: zod_1.z.string({
+      required_error: "Email is required",
     }),
+    password: zod_1.z.string({
+      required_error: "Password is required",
+    }),
+  }),
 });
 exports.refreshTokenZodSchema = zod_1.z.object({
-    cookies: zod_1.z.object({
-        refreshToken: zod_1.z.string({
-            required_error: "Refresh Token is required",
-        }),
+  cookies: zod_1.z.object({
+    refreshToken: zod_1.z.string({
+      required_error: "Refresh Token is required",
     }),
+  }),
 });
--- dist/app/modules/category/category.validation.js
+++ dist/app/modules/category/category.validation.js
@@ -3,14 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.updateCategoryZodSchema = exports.createCategoryZodSchema = void 0;
 const zod_1 = require("zod");
 exports.createCategoryZodSchema = zod_1.z.object({
-    body: zod_1.z.object({
-        name: zod_1.z.string({
-            required_error: "Name is required",
-        }),
+  body: zod_1.z.object({
+    name: zod_1.z.string({
+      required_error: "Name is required",
     }),
+  }),
 });
 exports.updateCategoryZodSchema = zod_1.z.object({
-    body: zod_1.z.object({
-        name: zod_1.z.string().optional(),
-    }),
+  body: zod_1.z.object({
+    name: zod_1.z.string().optional(),
+  }),
 });
