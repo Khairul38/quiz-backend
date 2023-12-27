05a6b416cfffe627243d61c499dc53f902eab84d
Khairul Alam <khairulislam38@gmail.com>
2023-12-27 12:58:26 +0600

    requirement update

--- dist/app/modules/auth/auth.service.js
+++ dist/app/modules/auth/auth.service.js
@@ -1,47 +1,18 @@
 "use strict";
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator["throw"](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done
-          ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.refreshTokenUserFromDB =
-  exports.loginUserFromDB =
-  exports.createUserToDB =
-    void 0;
+exports.refreshTokenUserFromDB = exports.loginUserFromDB = exports.createUserToDB = void 0;
 const bcrypt_1 = __importDefault(require("bcrypt"));
 const http_status_1 = __importDefault(require("http-status"));
 const config_1 = __importDefault(require("../../../config"));
@@ -50,145 +21,94 @@ const jwtHelpers_1 = require("../../../helpers/jwtHelpers");
 const passwordMatchHelper_1 = require("../../../helpers/passwordMatchHelper");
 // import { prismaExclude } from "../../../helpers/prismaExcludeHelper";
 const prisma_1 = require("../../../shared/prisma");
-const createUserToDB = userData =>
-  __awaiter(void 0, void 0, void 0, function* () {
+const createUserToDB = (userData) => __awaiter(void 0, void 0, void 0, function* () {
     const isUserExist = yield prisma_1.prisma.user.findUnique({
-      where: {
-        email: userData.email,
-      },
+        where: {
+            email: userData.email,
+        },
     });
     if (isUserExist) {
-      throw new ApiError_1.default(
-        http_status_1.default.BAD_REQUEST,
-        "User already exist with this email"
-      );
+        throw new ApiError_1.default(http_status_1.default.BAD_REQUEST, "User already exist with this email");
     }
     // hash password before create
-    userData.password = yield bcrypt_1.default.hash(
-      userData.password,
-      Number(config_1.default.bcrypt_salt_rounds)
-    );
+    userData.password = yield bcrypt_1.default.hash(userData.password, Number(config_1.default.bcrypt_salt_rounds));
     const createdUser = yield prisma_1.prisma.user.create({
-      data: userData,
+        data: userData,
     });
     if (createdUser) {
-      //create access token & refresh token
-      const { id, role, name, email, contactNo, address, profileImg } =
-        createdUser;
-      const accessToken = (0, jwtHelpers_1.createToken)(
-        { id, role, email, name, contactNo, address, profileImg },
-        config_1.default.jwt.secret,
-        config_1.default.jwt.expires_in
-      );
-      const refreshToken = (0, jwtHelpers_1.createToken)(
-        { id, role, email, name, contactNo, address, profileImg },
-        config_1.default.jwt.refresh_secret,
-        config_1.default.jwt.refresh_expires_in
-      );
-      return {
-        accessToken,
-        refreshToken,
-      };
-      // const userWithoutPassword = prismaExclude<User, "password">(createdUser, [
-      //   "password",
-      // ]);
-      // return userWithoutPassword;
-    } else {
-      throw new ApiError_1.default(
-        http_status_1.default.BAD_REQUEST,
-        "Failed to create user!"
-      );
+        //create access token & refresh token
+        const { id, role, name, email, contactNo, address, profileImg } = createdUser;
+        const accessToken = (0, jwtHelpers_1.createToken)({ id, role, email, name, contactNo, address, profileImg }, config_1.default.jwt.secret, config_1.default.jwt.expires_in);
+        const refreshToken = (0, jwtHelpers_1.createToken)({ id, role, email, name, contactNo, address, profileImg }, config_1.default.jwt.refresh_secret, config_1.default.jwt.refresh_expires_in);
+        return {
+            accessToken,
+            refreshToken,
+        };
+        // const userWithoutPassword = prismaExclude<User, "password">(createdUser, [
+        //   "password",
+        // ]);
+        // return userWithoutPassword;
     }
-  });
+    else {
+        throw new ApiError_1.default(http_status_1.default.BAD_REQUEST, "Failed to create user!");
+    }
+});
 exports.createUserToDB = createUserToDB;
-const loginUserFromDB = payload =>
-  __awaiter(void 0, void 0, void 0, function* () {
+const loginUserFromDB = (payload) => __awaiter(void 0, void 0, void 0, function* () {
     const { email, password } = payload;
     const isUserExist = yield prisma_1.prisma.user.findUnique({
-      where: {
-        email,
-      },
+        where: {
+            email,
+        },
     });
     if (!isUserExist) {
-      throw new ApiError_1.default(
-        http_status_1.default.NOT_FOUND,
-        "User does not exist with this email"
-      );
+        throw new ApiError_1.default(http_status_1.default.NOT_FOUND, "User does not exist with this email");
     }
-    if (
-      isUserExist.password &&
-      !(yield (0, passwordMatchHelper_1.isPasswordMatched)(
-        password,
-        isUserExist.password
-      ))
-    ) {
-      throw new ApiError_1.default(
-        http_status_1.default.UNAUTHORIZED,
-        "Password is incorrect"
-      );
+    if (isUserExist.password &&
+        !(yield (0, passwordMatchHelper_1.isPasswordMatched)(password, isUserExist.password))) {
+        throw new ApiError_1.default(http_status_1.default.UNAUTHORIZED, "Password is incorrect");
     }
     //create access token & refresh token
     const { id, role, name, contactNo, address, profileImg } = isUserExist;
-    const accessToken = (0, jwtHelpers_1.createToken)(
-      { id, role, email, name, contactNo, address, profileImg },
-      config_1.default.jwt.secret,
-      config_1.default.jwt.expires_in
-    );
-    const refreshToken = (0, jwtHelpers_1.createToken)(
-      { id, role, email, name, contactNo, address, profileImg },
-      config_1.default.jwt.refresh_secret,
-      config_1.default.jwt.refresh_expires_in
-    );
+    const accessToken = (0, jwtHelpers_1.createToken)({ id, role, email, name, contactNo, address, profileImg }, config_1.default.jwt.secret, config_1.default.jwt.expires_in);
+    const refreshToken = (0, jwtHelpers_1.createToken)({ id, role, email, name, contactNo, address, profileImg }, config_1.default.jwt.refresh_secret, config_1.default.jwt.refresh_expires_in);
     return {
-      accessToken,
-      refreshToken,
+        accessToken,
+        refreshToken,
     };
-  });
+});
 exports.loginUserFromDB = loginUserFromDB;
-const refreshTokenUserFromDB = token =>
-  __awaiter(void 0, void 0, void 0, function* () {
+const refreshTokenUserFromDB = (token) => __awaiter(void 0, void 0, void 0, function* () {
     //verify token
     // invalid token - synchronous
     let verifiedToken = null;
     try {
-      verifiedToken = (0, jwtHelpers_1.verifyToken)(
-        token,
-        config_1.default.jwt.refresh_secret
-      );
-    } catch (err) {
-      throw new ApiError_1.default(
-        http_status_1.default.FORBIDDEN,
-        "Invalid Refresh Token"
-      );
+        verifiedToken = (0, jwtHelpers_1.verifyToken)(token, config_1.default.jwt.refresh_secret);
+    }
+    catch (err) {
+        throw new ApiError_1.default(http_status_1.default.FORBIDDEN, "Invalid Refresh Token");
     }
     console.log(verifiedToken);
     const { email, name } = verifiedToken;
     // tumi delete hye gso  kintu tumar refresh token ase
     // checking deleted user's refresh token
     const isUserExist = yield prisma_1.prisma.user.findUnique({
-      where: {
-        email,
-      },
+        where: {
+            email,
+        },
     });
     if (!isUserExist) {
-      throw new ApiError_1.default(
-        http_status_1.default.NOT_FOUND,
-        "User does not exist"
-      );
+        throw new ApiError_1.default(http_status_1.default.NOT_FOUND, "User does not exist");
     }
     //generate new token
-    const newAccessToken = (0, jwtHelpers_1.createToken)(
-      {
+    const newAccessToken = (0, jwtHelpers_1.createToken)({
         id: isUserExist.id,
         role: isUserExist.role,
         email,
         name,
-      },
-      config_1.default.jwt.secret,
-      config_1.default.jwt.expires_in
-    );
+    }, config_1.default.jwt.secret, config_1.default.jwt.expires_in);
     return {
-      accessToken: newAccessToken,
+        accessToken: newAccessToken,
     };
-  });
+});
 exports.refreshTokenUserFromDB = refreshTokenUserFromDB;
--- dist/app/modules/leaderBoard/leaderBoard.route.js
+++ dist/app/modules/leaderBoard/leaderBoard.route.js
@@ -1,68 +1,19 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.LeaderBoardRoutes = void 0;
 const express_1 = __importDefault(require("express"));
 const user_1 = require("../../../enums/user");
 const auth_1 = __importDefault(require("../../middlewares/auth"));
-const validateRequest_1 = __importDefault(
-  require("../../middlewares/validateRequest")
-);
+const validateRequest_1 = __importDefault(require("../../middlewares/validateRequest"));
 const leaderBoard_controller_1 = require("./leaderBoard.controller");
 const leaderBoard_validation_1 = require("./leaderBoard.validation");
 const router = express_1.default.Router();
-router.post(
-  "/create-leaderBoard",
-  (0, auth_1.default)(
-    user_1.ENUM_USER_ROLE.SUPER_ADMIN,
-    user_1.ENUM_USER_ROLE.ADMIN,
-    user_1.ENUM_USER_ROLE.PERFORMER
-  ),
-  (0, validateRequest_1.default)(
-    leaderBoard_validation_1.createLeaderBoardZodSchema
-  ),
-  leaderBoard_controller_1.createLeaderBoard
-);
-router.get(
-  "/",
-  (0, auth_1.default)(
-    user_1.ENUM_USER_ROLE.SUPER_ADMIN,
-    user_1.ENUM_USER_ROLE.ADMIN,
-    user_1.ENUM_USER_ROLE.PERFORMER
-  ),
-  leaderBoard_controller_1.getAllLeaderBoard
-);
-router.get(
-  "/:id",
-  (0, auth_1.default)(
-    user_1.ENUM_USER_ROLE.SUPER_ADMIN,
-    user_1.ENUM_USER_ROLE.ADMIN,
-    user_1.ENUM_USER_ROLE.PERFORMER
-  ),
-  leaderBoard_controller_1.getSingleLeaderBoard
-);
-router.patch(
-  "/:id",
-  (0, auth_1.default)(
-    user_1.ENUM_USER_ROLE.SUPER_ADMIN,
-    user_1.ENUM_USER_ROLE.ADMIN,
-    user_1.ENUM_USER_ROLE.PERFORMER
-  ),
-  (0, validateRequest_1.default)(
-    leaderBoard_validation_1.updateLeaderBoardZodSchema
-  ),
-  leaderBoard_controller_1.updateSingleLeaderBoard
-);
-router.delete(
-  "/:id",
-  (0, auth_1.default)(
-    user_1.ENUM_USER_ROLE.SUPER_ADMIN,
-    user_1.ENUM_USER_ROLE.ADMIN
-  ),
-  leaderBoard_controller_1.deleteSingleLeaderBoard
-);
+router.post("/create-leaderBoard", (0, auth_1.default)(user_1.ENUM_USER_ROLE.SUPER_ADMIN, user_1.ENUM_USER_ROLE.ADMIN, user_1.ENUM_USER_ROLE.PERFORMER), (0, validateRequest_1.default)(leaderBoard_validation_1.createLeaderBoardZodSchema), leaderBoard_controller_1.createLeaderBoard);
+router.get("/", (0, auth_1.default)(user_1.ENUM_USER_ROLE.SUPER_ADMIN, user_1.ENUM_USER_ROLE.ADMIN, user_1.ENUM_USER_ROLE.PERFORMER), leaderBoard_controller_1.getAllLeaderBoard);
+router.get("/:id", (0, auth_1.default)(user_1.ENUM_USER_ROLE.SUPER_ADMIN, user_1.ENUM_USER_ROLE.ADMIN, user_1.ENUM_USER_ROLE.PERFORMER), leaderBoard_controller_1.getSingleLeaderBoard);
+router.patch("/:id", (0, auth_1.default)(user_1.ENUM_USER_ROLE.SUPER_ADMIN, user_1.ENUM_USER_ROLE.ADMIN, user_1.ENUM_USER_ROLE.PERFORMER), (0, validateRequest_1.default)(leaderBoard_validation_1.updateLeaderBoardZodSchema), leaderBoard_controller_1.updateSingleLeaderBoard);
+router.delete("/:id", (0, auth_1.default)(user_1.ENUM_USER_ROLE.SUPER_ADMIN, user_1.ENUM_USER_ROLE.ADMIN), leaderBoard_controller_1.deleteSingleLeaderBoard);
 exports.LeaderBoardRoutes = router;
